b315c8f2aebe95dbca9d2f6b50a798374f768db5
Melissa Kaulfuss <melissacor3lfuss@gmail.com>
2017-11-02 11:51:51 +1300

    Remove whitespace

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,5 +1,4 @@
 class StringCalculator
-
   def add(*args)
     if args.compact.empty?
       0
@@ -7,5 +6,4 @@ class StringCalculator
       args.inject(0) { | sum, arg | sum += arg.to_i  }
     end
   end
-
 end
